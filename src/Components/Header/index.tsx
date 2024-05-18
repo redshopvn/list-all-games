84ff73efe52978bdc38b5dc45a72b6c38f15f6a6
redshopvn <141570735+redshopvn@users.noreply.github.com>
2024-05-18 22:55:11 +0700

    edit img

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,9 @@
 
 
 const Header = () => {
-	return <div><img src={'free_banner.png'}  alt="A bird sitting on a nest of eggs." /></div>
+	return <div><img src={'bannerbig.webp'} height={149} width={1140} alt="2051 Game Bản Quyền Miễn Phí Cho Khách Hàng REDshop.vn." />
+
+		</div>
         
 	;
 }
