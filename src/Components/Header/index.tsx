a7a0bb0800078f23051ea37a11a7d6a82701994d
HungBM <hungbmit@gmail.com>
2024-08-06 00:15:54 +0700

    fix other

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,7 @@
 
 
 const Header = () => {
-	return <div><img src={'bannerbig.webp'} height={149} width={1140} alt="2051 Game Bản Quyền Miễn Phí Cho Khách Hàng REDshop.vn." />
+	return <div><img src={'bannerbig.webp'} height={149} width={1140} alt="2081 Game Bản Quyền Miễn Phí Cho Khách Hàng REDshop.vn." />
 
 		</div>
         
