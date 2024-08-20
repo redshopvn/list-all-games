e1244f4a49c19ffc4af53dfedcfed7dd1bb9bdb6
Bui Hung <141570735+redshopvn@users.noreply.github.com>
2024-08-20 09:26:32 +0700

    Update index.tsx

--- src/Components/NewGames/index.tsx
+++ src/Components/NewGames/index.tsx
@@ -3,14 +3,14 @@ import AllEpicGames from '../../epic_games.json'
 const NewGames = () => {
     const allGames = [...AllSteamGames, ...AllEpicGames]
 
-    return (<div className="card_new">
+    return (<div className="card">
         <p className='message'>🌟🌟🌟GAME MỚI🌟🌟🌟</p><div id="list_game_container" className='list_game_container_new'>
             {allGames.map((g: any, i) =>
                 g.tags && g.tags.includes('new_20240805') && <div className='ig ' key={`game-${g.name}-${i}`}><img
                     alt={g.name}
                     height={195}
                     title={g.name}
-                    className={g.tags.includes('highlight') ? 'shimmer' : ''}
+                    className={g.tags.includes('highlight') ? '' : ''}
                     src={g.image} // use normal <img> attributes as props
                     width={135} />
                     <div className={g.tags.includes('highlight') ? ' title' : 'title'}>{g.name}</div>
