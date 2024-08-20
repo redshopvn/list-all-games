c3c03c2e27c0f8706b5cd79b3ca6326b34a30ca0
Bui Hung <141570735+redshopvn@users.noreply.github.com>
2024-08-20 09:20:04 +0700

    add flash effect

--- src/Components/NewGames/index.tsx
+++ src/Components/NewGames/index.tsx
@@ -4,19 +4,19 @@ const NewGames = () => {
     const allGames = [...AllSteamGames, ...AllEpicGames]
 
     return (<div className="card_new">
-        <p className='message'>Game Mới</p><div id="list_game_container" className='list_game_container_new'>
-        {allGames.map((g: any, i) =>
-            g.tags && g.tags.includes('new_20240805') && <div className='ig' key={`game-${g.name}-${i}`}><img
-                alt={g.name}
-                height={195}
-                title={g.name}
-                className={g.tags.includes('highlight') ? 'highlight_game' :  ''}
-                src={g.image} // use normal <img> attributes as props
-                width={135} />
-                <div className={g.tags.includes('highlight') ? 'highlight_game_title title' :  'title'}>{g.name}</div>
-            </div>
-        )}
-    </div></div>)
+        <p className='message'>🌟🌟🌟GAME MỚI🌟🌟🌟</p><div id="list_game_container" className='list_game_container_new'>
+            {allGames.map((g: any, i) =>
+                g.tags && g.tags.includes('new_20240805') && <div className='ig ' key={`game-${g.name}-${i}`}><img
+                    alt={g.name}
+                    height={195}
+                    title={g.name}
+                    className={g.tags.includes('highlight') ? 'shimmer' : ''}
+                    src={g.image} // use normal <img> attributes as props
+                    width={135} />
+                    <div className={g.tags.includes('highlight') ? ' title' : 'title'}>{g.name}</div>
+                </div>
+            )}
+        </div></div>)
 
         ;
 }
