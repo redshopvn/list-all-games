81cfde99ea7ca8fd33d9574cf87287cf0fc479fa
Bui Hung <141570735+redshopvn@users.noreply.github.com>
2024-08-20 09:30:28 +0700

    chagne to lazy load

--- src/Components/AllGames/index.tsx
+++ src/Components/AllGames/index.tsx
@@ -1,16 +1,17 @@
 import AllSteamGames from '../../steam_games.json'
 import AllEpicGames from '../../epic_games.json'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 const AllGames = () => {
     const allGames = [...AllSteamGames, ...AllEpicGames]
 
     return (<div id="list_game_container" className='list_game_container'>
         {allGames.map((g: any, i) =>
-            !g.tags && <div className='ig' key={`game-${g.name}-${i}`}><img
-                alt={g.name}
-                height={195}
-                title={g.name}
-                src={g.image} // use normal <img> attributes as props
-                width={135} />
+            !g.tags && <div className='ig' key={`game-${g.name}-${i}`}>
+                <LazyLoadImage
+                    alt={g.name}
+                    height={195}
+                    src={g.image} // use normal <img> attributes as props
+                    width={135} />
                 <div className="title">{g.name}</div>
             </div>
         )}
--- src/Components/NewGames/index.tsx
+++ src/Components/NewGames/index.tsx
@@ -1,18 +1,18 @@
 import AllSteamGames from '../../steam_games.json'
 import AllEpicGames from '../../epic_games.json'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 const NewGames = () => {
     const allGames = [...AllSteamGames, ...AllEpicGames]
 
     return (<div className="card">
         <p className='message'>🌟🌟🌟GAME MỚI🌟🌟🌟</p><div id="list_game_container" className='list_game_container_new'>
             {allGames.map((g: any, i) =>
-                g.tags && g.tags.includes('new_20240805') && <div className='ig ' key={`game-${g.name}-${i}`}><img
-                    alt={g.name}
-                    height={195}
-                    title={g.name}
-                    className={g.tags.includes('highlight') ? '' : ''}
-                    src={g.image} // use normal <img> attributes as props
-                    width={135} />
+                g.tags && g.tags.includes('new_20240805') && <div className='ig ' key={`game-${g.name}-${i}`}>
+                    <LazyLoadImage
+                        alt={g.name}
+                        height={195}
+                        src={g.image} // use normal <img> attributes as props
+                        width={135} />
                     <div className={g.tags.includes('highlight') ? ' title' : 'title'}>{g.name}</div>
                 </div>
             )}
